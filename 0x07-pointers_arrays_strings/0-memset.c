6e0a9138ed91a96af5fd05d6bd0c8d431f4f827d
Pweshie18 <preciousidio@gmail.com>
2022-09-26 22:33:07 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,14 +5,18 @@
  * _memset - Entry point
  * @s: pointed destination
  * @b: constant byte
- * @n: bytes
- * Return: Always 0 (Success)
+ * @n: memory area pointed to by s
+ *
+ * Return: pointer to the memory area
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i 0; i < n; i++)
+	for (i = 0; i < n; i++)
+	{
 		s[i] = b;
+	}
+
 	return (s);
 }
